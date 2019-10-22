ff071df6a16f9e3b7bf2ecbe860ef6db78c1eafd
efnushtaev <efnushtaev@gmail.com>
2019-10-23 00:35:11 +0300

    20 lesson

--- src/View/Components/Navbar/Navbar.jsx
+++ src/View/Components/Navbar/Navbar.jsx
@@ -1,20 +1,21 @@
 import React from 'react';
-import classes from './Navbar.module.css'
+import classes from './Navbar.module.css';
+import {NavLink} from 'react-router-dom';
 
 let Navbar = () => {
     return (
         <nav className={classes.navigation}>
-            <div>
-                <a href='/profile'>Profile</a>
+            <div className={classes.item}>
+                <NavLink activeClassName={classes.active} to='/profile'>Profile</NavLink>
             </div>
-            <div>
-                <a href='/dialogs'>Messages</a>
+            <div className={classes.item}>
+                <NavLink activeClassName={classes.active} to='/dialogs'>Messages</NavLink>
             </div>
-            <div>
-                <a href='./news'>News</a>
+            <div className={classes.item}>
+                <NavLink activeClassName={classes.active} to='./news'>News</NavLink>
             </div>
-            <div>
-                <a href='/music'>Music</a>
+            <div className={classes.item}>
+                <NavLink activeClassName={classes.active} to='/music'>Music</NavLink>
             </div>
             <div>
                 <a>Settings</a>

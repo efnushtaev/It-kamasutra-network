8f1addd7a28c026d9d4978471fa5374f6813b50f
efnushtaev <efnushtaev@gmail.com>
2019-11-14 00:43:55 +0300

    26 lesson

--- src/View/Components/MainContent/Profile/MyPosts/MyPosts.jsx
+++ src/View/Components/MainContent/Profile/MyPosts/MyPosts.jsx
@@ -2,15 +2,12 @@ import React from 'react';
 import Post from './Post/Post';
 import classes from './MyPosts.module.css';
 
-const MyPosts = () => {
-    let postsData = [
-        { id: 1, message: 'Hi! this is a first post', likesCount: '23' },
-        { id: 2, message: 'And this is a second one', likesCount: '11' }
-    ]
+const MyPosts = (props) => {
 
     let postElements =
-        postsData.map(p => <Post message={p.message} likesCount={p.likesCount} />)
-    return (
+        props.postsData.map(p => <Post message={p.message} likesCount={p.likesCount} />)
+    
+        return (
         <div className={classes.container}>
             <textarea></textarea>
             <button>Add post</button>
--- src/View/Components/MainContent/Profile/Profile.jsx
+++ src/View/Components/MainContent/Profile/Profile.jsx
@@ -4,10 +4,16 @@ import MyPosts from './MyPosts/MyPosts';
 import ProfileInfo from './ProfileInfo/ProfileInfo';
 
 let Profile = () => {
+
+    let postsData = [
+        { id: 1, message: 'Hi! this is a first post', likesCount: '23' },
+        { id: 2, message: 'And this is a second one', likesCount: '11' }
+    ]
+
     return (
         <main>
            <ProfileInfo />
-           <MyPosts/>
+           <MyPosts postsData={postsData}/>
         </main>
     )
 }

b5511b6f2f96de07fa757758f92de1b29d03c7e0
efnushtaev <efnushtaev@gmail.com>
2020-03-06 08:13:29 +0300

    71 lesson

--- src/View/Components/MainContent/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/View/Components/MainContent/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import classes from './ProfileInfo.module.css';
 import Preloader from '../../../Common/Preloader';
+import ProfileStatus from './ProfileStatus'
 
 let ProfileInfo = (props) => {
     if(!props.profile){
@@ -15,6 +16,7 @@ let ProfileInfo = (props) => {
                 ? props.profile.photos.large 
                 : 'https://windows10free.ru/uploads/posts/2017-04/1493287748_1487679899_icon-user-640x640.png'} />
             </div>
+            <ProfileStatus status="Hello" />
             <div className={classes.description}>
             </div>
         </div>

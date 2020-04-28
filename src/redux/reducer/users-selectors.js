e593122beef2eeb1a45c65a871f25da2fc84cb78
efnushtaev <efnushtaev@gmail.com>
2020-04-28 09:09:09 +0300

    83 lesson

--- src/redux/reducer/users-selectors.js
+++ src/redux/reducer/users-selectors.js
@@ -1,6 +1,10 @@
-export const getUsers = state => state.usersPage.users;
+import {createSelector} from 'reselect';
+
+const getUsersSelector = state => state.usersPage.users;
 export const getPagesSize = state => state.usersPage.pagesSize;
 export const getTotalUsersCount = state => state.usersPage.totalUsersCount;
 export const getCurrentPage = state => state.usersPage.currentPage;
 export const getIsFetching = state => state.usersPage.isFetching;
 export const getFollowingProgress = state => state.usersPage.followingProgress;
+
+export const getUsers = createSelector( getUsersSelector, (users) => users.filter(u => true) )

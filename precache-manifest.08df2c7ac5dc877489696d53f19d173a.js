2cb7726de6a75e2724742db0b5562563d410643c
efnushtaev <efnushtaev@gmail.com>
2020-05-15 09:10:28 +0300

    Updates

---
+++ precache-manifest.08df2c7ac5dc877489696d53f19d173a.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a772c95998e5da1b93866131ff2dbe3e",
+    "url": "/it-kamasutra-network/index.html"
+  },
+  {
+    "revision": "5c0df59b113ce11b45d4",
+    "url": "/it-kamasutra-network/static/css/3.50c72f87.chunk.css"
+  },
+  {
+    "revision": "12c31e892f08cf63b61e",
+    "url": "/it-kamasutra-network/static/css/4.fce3df3a.chunk.css"
+  },
+  {
+    "revision": "edcf4bb365ede87c1ceb",
+    "url": "/it-kamasutra-network/static/css/5.361712cd.chunk.css"
+  },
+  {
+    "revision": "9e87ab2c441005edb36f",
+    "url": "/it-kamasutra-network/static/css/main.af28d617.chunk.css"
+  },
+  {
+    "revision": "257b243bb50134796a12",
+    "url": "/it-kamasutra-network/static/js/2.874af5f9.chunk.js"
+  },
+  {
+    "revision": "5c0df59b113ce11b45d4",
+    "url": "/it-kamasutra-network/static/js/3.6868e996.chunk.js"
+  },
+  {
+    "revision": "12c31e892f08cf63b61e",
+    "url": "/it-kamasutra-network/static/js/4.d961ed59.chunk.js"
+  },
+  {
+    "revision": "edcf4bb365ede87c1ceb",
+    "url": "/it-kamasutra-network/static/js/5.8e8bd64f.chunk.js"
+  },
+  {
+    "revision": "9e87ab2c441005edb36f",
+    "url": "/it-kamasutra-network/static/js/main.1da463f8.chunk.js"
+  },
+  {
+    "revision": "697e527f099c27f834ea",
+    "url": "/it-kamasutra-network/static/js/runtime~main.67b003a4.js"
+  },
+  {
+    "revision": "c8c3e97a3c988fef6d3aa5518965fc84",
+    "url": "/it-kamasutra-network/static/media/ava.c8c3e97a.png"
+  },
+  {
+    "revision": "cf4c8e5a20d86857d09e17db23470e3f",
+    "url": "/it-kamasutra-network/static/media/header-bg.cf4c8e5a.png"
+  }
+]);
